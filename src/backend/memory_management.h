64594f7091999aabcb559a93884a495f1b70532a
Guyed Riley <guybriley02@gmail.com>
2025-01-21 03:01:21 -0700

    Create memory_management.h

---
+++ src/backend/memory_management.h
@@ -0,0 +1,30 @@
+c
+
+#ifndef MEMORY_MANAGEMENT_H
+#define MEMORY_MANAGEMENT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+
+/**
+ * \brief Allocates a block of memory aligned to 'alignment' bytes. 
+ * \param size Size in bytes
+ * \param alignment Alignment in bytes (must be power of 2)
+ * \return Pointer to aligned memory or NULL on failure
+ */
+void* aligned_malloc(size_t size, size_t alignment);
+
+/**
+ * \brief Frees memory allocated by aligned_malloc.
+ * \param ptr Pointer to memory block
+ */
+void aligned_free(void* ptr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MEMORY_MANAGEMENT_H */
